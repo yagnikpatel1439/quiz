c9f9a37edbac0c553b936dd5dfcf8f294e8b2bb3
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2021-03-21 20:34:57 -0400

    setting up quiz app

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <b-nav>
+            <b-nav-item active>LetsQuiz </b-nav-item>
+            <b-nav-item>Counter: 4/10</b-nav-item>
+        </b-nav>
+    </div>
+</template>
